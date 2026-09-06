b7b952192067bf7aa6a8dbe1bc38711fe878b982
Aditya Agrahari <aditya.agrahari.849@gmail.com>
2026-09-06 15:17:13 +0530

    Implement authentication routes and middleware for user verification

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,9 @@ import { clerkMiddleware } from '@clerk/express'
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
+
 import clerkWebhook from "./webhooks/clerk.webhook.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -28,6 +30,8 @@ app.use(clerkMiddleware());
 app.get("/health",(req,res)=>{  
     res.status(200).json({ok:true});
 });
+
+app.use("/api/auth",authRoutes)
 
 if(fs.existsSync(publicDir)){
     app.use(express.static(publicDir));
