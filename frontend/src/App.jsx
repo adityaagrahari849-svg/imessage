abc1ac79bbd580da3e636a6c0c8577026f8c216c
Aditya Agrahari <aditya.agrahari.849@gmail.com>
2026-09-07 23:40:04 +0530

    make the folder of heroui and saves keystrokes, theme and wallpaper

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
-import './App.css'
 import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { Button } from '@heroui/react';
 
-  function App(){
+
+function App() {
   return (
     <div>
-      <h1>My App</h1>
+      <h1 className="text-4xl text-red-500 bg-blue-400">My App</h1>
+      <Button>
+        My Button
+      </Button>
 
       <header>
         <Show when="signed-out">
-          <SignInButton mode="modal"/>
+          <SignInButton mode="modal" />
           <SignUpButton mode="modal" />
         </Show>
         <Show when="signed-in">
