0a044ef75692b9ef6f155d9115d7dc19a2a839f5
Aditya Agrahari <aditya.agrahari.849@gmail.com>
2026-09-09 01:45:46 +0530

    updated main.jsx to include BrowserRouter for routing Created AuthPage and ChatPage components for future routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,23 @@
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
-import { Button } from '@heroui/react';
-
+import { WallpaperProvider } from './context/WallpaperContext';
+import { ThemeProvider } from './context/ThemeContext';
+import { Navigate, Route,Routes } from "react-router";
+import ChatPage from './pages/ChatPage';
+import AuthPage from './pages/AuthPage';
+import { useAuth } from '@clerk/react';
 
 function App() {
-  return (
-    <div>
-      <h1 className="text-4xl text-red-500 bg-blue-400">My App</h1>
-      <Button>
-        My Button
-      </Button>
+const { isSignedIn, isLoaded} = useAuth();
 
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
+if(!isLoaded) return <p>loading...</p>;
+  return (
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
+          <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/chat"} replace />}/>
+        </Routes>
+      </WallpaperProvider>
+    </ThemeProvider>
   );
 }
 export default App;
