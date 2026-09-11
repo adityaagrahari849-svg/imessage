6130f9ce2a1b0c107463eb624a9d8938842d458b
Aditya Agrahari <aditya.agrahari.849@gmail.com>
2026-09-11 22:58:04 +0530

    Implemented PageLoader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,21 @@
 import { WallpaperProvider } from './context/WallpaperContext';
 import { ThemeProvider } from './context/ThemeContext';
-import { Navigate, Route,Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import ChatPage from './pages/ChatPage';
 import AuthPage from './pages/AuthPage';
 import { useAuth } from '@clerk/react';
-// import PageLoader from "./components/PageLoader";
+import PageLoader from './components/PageLoader';
 
 function App() {
-const { isSignedIn, isLoaded} = useAuth();
+  const { isSignedIn, isLoaded } = useAuth();
 
-if(!isLoaded) return <p>Loading...</p>;
+  if (!isLoaded) return <PageLoader />;
   return (
     <ThemeProvider>
       <WallpaperProvider>
         <Routes>
           <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
-          <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}/>
+          <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />} />
         </Routes>
       </WallpaperProvider>
     </ThemeProvider>
