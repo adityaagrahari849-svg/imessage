6399ea722482465ad6cca33a199352476193d39c
Aditya Agrahari <aditya.agrahari.849@gmail.com>
2026-09-12 23:21:43 +0530

    implement auth store with socket integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,29 @@ import ChatPage from './pages/ChatPage';
 import AuthPage from './pages/AuthPage';
 import { useAuth } from '@clerk/react';
 import PageLoader from './components/PageLoader';
+import { useEffect } from 'react';
+import { useAuthStore } from './store/useAuthStore';
+import {Toaster} from "react-hot-toast";
 
 function App() {
   const { isSignedIn, isLoaded } = useAuth();
 
-  if (!isLoaded) return <PageLoader />;
+  //option 1
+  //const {checkAuth, isCheckingAuth,, clearAuth } = useAuthStore();
+
+  // option 2 - better for performance
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+  useEffect(() => {
+    if(!isLoaded) return;
+
+    if(isSignedIn) checkAuth();
+    else clearAuth();
+  },[checkAuth,clearAuth,isLoaded,isSignedIn]);
+
+  if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
   return (
     <ThemeProvider>
       <WallpaperProvider>
@@ -17,6 +35,7 @@ function App() {
           <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
           <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />} />
         </Routes>
+        <Toaster/>
       </WallpaperProvider>
     </ThemeProvider>
   );
