5a22a97076cf30f83f3b8dd8184698fcee102a7c
Aditya Agrahari <aditya.agrahari.849@gmail.com>
2026-09-19 18:55:16 +0530

    Temporarily changing protectRoute

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,25 +1,39 @@
 import { getAuth } from "@clerk/express";
 import User from "../models/user.model.js";
 
-export async function protectRoute(req,res,next) {
+export async function protectRoute(req, res, next) {
     try {
         const { userId } = getAuth(req);
 
-        if(!userId){
-            res.status(401).json({message:"Unauthorized"});
-            return ;
+        console.log("Clerk userId:", userId);
+
+        if (!userId) {
+            return res.status(401).json({
+                message: "Unauthorized"
+            });
         }
 
-        const user = await User.findOne({clerkId: userId});
-        if(!user){
-            res.status(404).json({message:"User profile is not synced yet"});
-            return;
+        const user = await User.findOne({ clerkId: userId });
+
+        console.log("Mongo user:", user);
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User profile is not synced yet"
+            });
         }
 
         req.user = user;
         next();
+
     } catch (error) {
-        console.error("Error in protectRoute middleware:",error.message);
-        res.status(500).json({message:"Internal server error"});
+        console.error(
+            "Error in protectRoute middleware:",
+            error.message
+        );
+
+        res.status(500).json({
+            message: "Internal server error"
+        });
     }
 }
